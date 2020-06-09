410df7b9b0c3ca26ce5c5ea4683209b272adb30d
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-09 17:15:46 +0100

    Exercise 1 completed

---
+++ 1-Functional_Components/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Amy Richards</h1>
+            <p>This is a paragraph...</p>
+            <ul>
+                <li>Charlotte</li>
+                <li>Amy</li>
+                <li>Taegan</li>
+            </ul>
+        </div>
+        )
+}
+
+export default MyInfo
