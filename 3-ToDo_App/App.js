8172fd53648da085cbc1954ee22d07090c92b609
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-10 20:32:14 +0100

    Changed to class component

--- 3-ToDo_App/App.js
+++ 3-ToDo_App/App.js
@@ -1,15 +1,24 @@
 import React from "react"
 
-import ToDoItem from "./ToDoItem"
+import TodoItem from "./ToDoItem"
 import todosData from "./todosData"
 
-function App() {
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
-    return (
-        <div className="todo-list">
-            {todoItems}
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos = todosData
+        }
+    }
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
 }
 
 export default App
