fdac587282c2975118119b2fafcabe51bf988b92
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-10 19:59:05 +0100

    Example 8 completed

--- 8-State/App.js
+++ 8-State/App.js
@@ -6,15 +6,17 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            answer: "Yes"
+            name: "Amy",
+            age: "23"
         }
     }
+
     render() {
         return (
             <div>
-                <h1>Is state important to know? {this.state.answer}</h1>
-                <ChildComponent answer={this.state.answer}/>
-            </div>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old</h3>
+            </div >
         )
     }
 }
