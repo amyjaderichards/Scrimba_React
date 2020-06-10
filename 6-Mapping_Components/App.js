d50f9e4394c8d62086dd8660897730903928dbc4
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-10 16:42:55 +0100

    Exercise 6 completed

--- 6-Mapping_Components/App.js
+++ 6-Mapping_Components/App.js
@@ -1,9 +1,14 @@
 import React from "react"
+import Product from "./Product"
 import productsData from "./vschoolProducts"
 
 function App() {
+    const productComponents = productsData.map(item => <Product key={item.id} product={item}/>)
+
     return(
-        <div></div>
+        <div>
+            {productComponents}
+        </div>
     )
 }
 
