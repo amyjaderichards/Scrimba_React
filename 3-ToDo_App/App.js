6b9339b50c26c342094044d1c56f35fc5114c6f3
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-10 12:10:18 +0100

    Basic version of todo list completed

--- 3-ToDo_App/App.js
+++ 3-ToDo_App/App.js
@@ -1,19 +1,13 @@
 import React from "react"
 
+import ToDoItem from "./ToDoItem"
+
 function App() {
     return (
-        <div>
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+        <div className="todo-list">
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
         </div>
     )
 }
