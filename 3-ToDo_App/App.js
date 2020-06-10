274e45bb3cf57c32034002fea6823938309255cc
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-10 16:41:50 +0100

    Props added

--- 3-ToDo_App/App.js
+++ 3-ToDo_App/App.js
@@ -1,13 +1,13 @@
 import React from "react"
 
 import ToDoItem from "./ToDoItem"
+import todosData from "./todosData"
 
 function App() {
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
     return (
         <div className="todo-list">
-        <ToDoItem />
-        <ToDoItem />
-        <ToDoItem />
+            {todoItems}
         </div>
     )
 }
