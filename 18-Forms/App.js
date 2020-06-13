cf83c1b3f673cae8bc78bc7d9009b33678dfe06b
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-13 17:28:00 +0100

    Exercise 18 completed

---
+++ 18-Forms/App.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            firstName: "",
+            lastName: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <form>
+                <input 
+                type="text" 
+                name="firstName"
+                placeholder="first name" 
+                onChange={this.handleChange}/>
+
+                <input type="text"
+                name="lastName"
+                placeholder="last name"
+                onChange={this.handleChange}/>
+
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
+        )
+    }
+}
+
+export default App
