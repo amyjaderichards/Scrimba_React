5b4a408979178871517a3d613955bc71e27622cd
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-14 14:05:50 +0100

    Exercise 21 completed

---
+++ 21-Container_Component_Architecture/FormContainer.js
@@ -0,0 +1,44 @@
+// https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
+
+import React, { Component } from "react"
+
+import FormComponent from "./FormComponent"
+
+class Form extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: "",
+            age: "",
+            gender: "",
+            destination: "",
+            isVegan: false,
+            isKosher: false,
+            isLactoseFree: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        const { name, value, type, checked } = event.target
+        type === "checkbox" ?
+            this.setState({
+                [name]: checked
+            })
+            :
+            this.setState({
+                [name]: value
+            })
+    }
+
+    render() {
+        return (
+            <FormComponent
+                handleChange={this.handleChange}
+                data={this.state}
+            />
+        )}
+}
+
+export default Form
