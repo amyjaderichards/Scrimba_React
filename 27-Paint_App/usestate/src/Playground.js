4175a14bb7529a69823b8ef90e73aeb965b1b894
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-21 15:59:01 +0100

    Initial commit

--- 27-Paint_App/usestate/src/Playground.js
+++ 27-Paint_App/usestate/src/Playground.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import randomColor from "randomcolor";
 
 function Playground() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState(null);
 
   function increment() {
       setCount(prevCount => prevCount + 1);
@@ -12,8 +13,12 @@ function Playground() {
       setCount(prevCount => prevCount - 1);
   }
 
+  useEffect(() => {
+    setColor(randomColor());
+  }, [count])
+
   return (
-    <div>
+    <div style={{ borderTop: `10px solid ${color}`}}>
       {count}
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
--- 27-Paint_App/usestate/src/app/components/Paint.js
+++ 27-Paint_App/usestate/src/app/components/Paint.js
@@ -1,12 +1,34 @@
-import React from "react"
-import Name from "./Name"
+import React from "react";
+import Name from "./Name";
+import ColorPicker from "./ColorPicker";
+import randomColor from "randomcolor";
 
 function Paint() {
-    return (
-        <div className="app">
-            <Name />
-        </div>
-    )
+  const getColors = () => {
+    const baseColor = randomColor().slice(1);
+    fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
+      .then((response) => response.json())
+      .then((response) => {
+        setColor(response.colors.map((color) => color.hex.value));
+        setActiveColor(response.colors[0].hex.value);
+      });
+  };
+
+  return (
+    <header style={{ borderTop: `10px solid ${activeColor}` }}>
+      <div className="app">
+        <Name />
+      </div>
+
+      <div style={{ marginTop: 10 }}>
+        <ColorPicker
+          colors={colors}
+          activeColor={activeColor}
+          setActiveColor={setActiveColor}
+        />
+      </div>
+    </header>
+  );
 }
 
 export default Paint;
