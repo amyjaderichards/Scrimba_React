9ba0954ced51ac5495e16472dd529a859741ba25
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-07-01 00:07:26 +0100

    Finished project

--- 27-Paint_App/paint_app/src/app/components/WindowSize.js
+++ 27-Paint_App/paint_app/src/app/components/WindowSize.js
@@ -1,28 +1,15 @@
 import React, { useState, useEffect } from 'react'
 
-export default function WindowSize() {
-    const [[windowWidth, windowHeight], setWindowSize] = useState([window.innerWidth, window.innerHeight])
-    const [visible, setVisible] = useState(false);
-
-    useEffect(() => {
-        let timeoutId
-        const handleResize = () => {
-            setWindowSize([window.innerWidth, window.innerHeight])
-            setVisible(true)
-            clearTimeout(timeoutId);
-            timeoutId = setTimeout(() => setVisible(false), 500)
-        }
-
-        window.addEventListener('resize', handleResize)
-        return () => window.removeEventListener('resize', handleResize)
-    }, [])
-
-    return (
-        <div className={`window-size ${visible ? '' : 'hidden'}`}>
-            {windowWidth} x {windowHeight}
-        </div>
-    )
-
-
-
+export default function useWindowSize(cb) {
+  const [[windowWidth, windowHeight], setWindowSize] = useState([window.innerWidth, window.innerHeight])
+  
+  useEffect(() => {
+    const handleResize = () => {
+      cb()
+      setWindowSize([window.innerWidth, window.innerHeight])
+    }
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+  return [windowWidth, windowHeight]
 }
