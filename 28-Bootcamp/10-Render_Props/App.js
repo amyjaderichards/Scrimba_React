663892009337623b2a12e42198a8859635c01666
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-07-10 18:32:22 +0100

    Exercise 10 completed

--- 28-Bootcamp/10-Render_Props/App.js
+++ 28-Bootcamp/10-Render_Props/App.js
@@ -6,16 +6,16 @@ import Example from "./Example";
 function App() {
   return (
     <div>
-      <Example render={
-          function(bool, number) {
-              return (
-                  <div>
-                      <h1>{number}</h1>
-                      <h1>{bool ? "true" : "false"}</h1>
-                      </div>
-              )
-          }
-      }/>
+      <Example
+        render={function (bool, number) {
+          return (
+            <div>
+              <h1>{number}</h1>
+              <h1>{bool ? "true" : "false"}</h1>
+            </div>
+          );
+        }}
+      />
     </div>
   );
 }
