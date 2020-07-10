471a9002584d422beb966756e338a99d12620781
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-07-10 18:32:13 +0100

    Exercise 6 completed

--- 28-Bootcamp/06-React_Children_2/App.js
+++ 28-Bootcamp/06-React_Children_2/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import InfoCallout from "./InfoCallout";
 import ImageCallout from "./ImageCallout";
 import EmailCallout from "./EmailCallout";
 import Callout from "./Callout";
@@ -11,11 +10,15 @@ function App() {
 
       <Callout>
         <h2>Don't miss out!</h2>
-        <p>
-          This is probably the best site you've ever come across. I'm glad
-          you're here to witness the magnificence of this website right now.
-        </p>
+        <p>Fill out the email form below</p>
       </Callout>
+
+      
+
+      <p>
+        This is probably the best site you've ever come across. I'm glad you're
+        here to witness the magnificence of this website right now.
+      </p>
 
       <ImageCallout
         img={"https://picsum.photos/id/102/4320/3240"}
