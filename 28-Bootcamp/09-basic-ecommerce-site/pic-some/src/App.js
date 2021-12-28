07bf9a841d530453059cfe002fd8e606b6245bdf
Amy <amyjaderichards@googlemail.com>
2021-12-28 17:02:20 +0000

    WIP react-router-dom

--- 28-Bootcamp/09-basic-ecommerce-site/pic-some/src/App.js
+++ 28-Bootcamp/09-basic-ecommerce-site/pic-some/src/App.js
@@ -1,15 +1,22 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 
 function App() {    
-    return (
-        <div>
-            <Header />
-            <h1>Home Page</h1>
-        </div>
-    )
+  return (
+      <div>
+          <Header />
+
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </BrowserRouter>
+      </div>
+  )
 }
 
 export default App
